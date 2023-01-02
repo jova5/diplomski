f3964ad42873eb73ce294ead852a4d34c2460140
jova5 <jovicicm55@outlook.com>
2023-01-02 14:31:39 +0100

    initial commit

---
+++ online-marketing-back/src/main/java/com/diplomski/onlinemarketingback/OnlineMarketingBackApplication.java
@@ -0,0 +1,13 @@
+package com.diplomski.onlinemarketingback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMarketingBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMarketingBackApplication.class, args);
+    }
+
+}
---
+++ online-marketing-back/src/test/java/com/diplomski/onlinemarketingback/OnlineMarketingBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplomski.onlinemarketingback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMarketingBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
