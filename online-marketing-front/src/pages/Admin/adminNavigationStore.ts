122d5392e641b2b491f9f36a8b67b29cf6ee26bd
jova5 <jovicicm55@outlook.com>
2023-01-05 21:04:30 +0100

    added Language component

--- online-marketing-front/src/pages/Admin/adminNavigationStore.ts
+++ online-marketing-front/src/pages/Admin/adminNavigationStore.ts
@@ -2,8 +2,9 @@ import {createSignal} from "solid-js";
 import Users from "./components/Users";
 import Stores from "./components/Stores";
 import Categories from "./components/Categories";
+import Language from "./components/Language";
 
-export const NavValues = ["Users", "Stores", "Categories"] as const;
+export const NavValues = ["Users", "Stores", "Categories", "Language"] as const;
 
 export type NavValue = typeof NavValues[number];
 
@@ -11,6 +12,7 @@ export const NavOptions = {
   Users: Users,
   Stores: Stores,
   Categories: Categories,
+  Language: Language
 }
 
 export const [nav, setNav] = createSignal<NavValue>(NavValues[0]);
---
+++ online-marketing-front/src/pages/Admin/components/Language.tsx
@@ -0,0 +1,9 @@
+import {Component} from "solid-js";
+
+const Language: Component = () => {
+  return(
+    <div>This is Language</div>
+  )
+}
+
+export default Language;
